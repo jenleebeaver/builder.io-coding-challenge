3e70acd290f3236675798b3b9a6cc3f5a8bfc709
Jennifer Beaver <jen.lee.beaver@gmail.com>
2023-03-01 15:09:55 -0800

    Create App.js

---
+++ App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+import PuppyGalleryPage from "./PuppyGalleryPage";
+
+export default function App() {
+  return (
+    <div className="App">
+      <PuppyGalleryPage></PuppyGalleryPage>
+    </div>
+  );
+}
